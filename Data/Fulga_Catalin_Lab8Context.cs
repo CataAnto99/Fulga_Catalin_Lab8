d688397ece795f507388c5b0d4173c73f55cd79a
CATALIN-ANTON FULGA <catalinfulga99@yahoo.com>
2021-11-22 13:02:17 +0200

    Add project files.

---
+++ Data/Fulga_Catalin_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Fulga_Catalin_Lab8.Models;
+
+namespace Fulga_Catalin_Lab8.Data
+{
+    public class Fulga_Catalin_Lab8Context : DbContext
+    {
+        public Fulga_Catalin_Lab8Context (DbContextOptions<Fulga_Catalin_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fulga_Catalin_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Fulga_Catalin_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Fulga_Catalin_Lab8.Models.Category> Category { get; set; }
+    }
+}
